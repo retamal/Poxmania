d6edcf301b5cc7337183bf75dcdb7012e8fea723
retamal <d.retamal@alumnos.urjc.es>
2015-03-16 12:51:54 +0100

    arreglado para mi

--- Poxmania/src/main/java/es/sidelab/Poxmania/Cesta.java
+++ Poxmania/src/main/java/es/sidelab/Poxmania/Cesta.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-package es.sidelab.Poxmania;
-/*pruebA git, he tocado*/
-import java.util.ArrayList; 
-/*hpña*/
-public class Cesta {
-	public ArrayList<Producto> cestaCompra = new ArrayList<Producto>();
-	
-	public Cesta(){
-		
-	}
-	
-	public void anadirProducto(Producto nuevoProducto){
-		
-		boolean metido=false;
-		for (Producto producto:cestaCompra){			
-			if (producto.getNombre().equalsIgnoreCase(nuevoProducto.getNombre())){
-				producto.setCantidad(producto.getCantidad()+1);
-				metido=true;
-			}
-		}
-		
-		if (!metido){
-			cestaCompra.add(nuevoProducto);
-		}
-		
-	}
-	
-	public void eliminarProducto(Producto viejoProducto){
-		cestaCompra.remove(viejoProducto);
-	}
-	
-	public Producto obtenerProducto(int posicion){
-		return cestaCompra.get(posicion);
-	}
-
-}
-=======
 package es.sidelab.Poxmania;
 /*pruebA git, he tocado. Pues ahora toco yo*/
 import java.util.ArrayList; 
@@ -72,4 +34,4 @@ public class Cesta {
 	}
 
 }
->>>>>>> origin/master
+
